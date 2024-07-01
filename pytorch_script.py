2e6d9fa5bd9c740fcc5ac2feab482eebe28157a6
Anshel Nohl <anshel.nohl@gmail.com>
2024-07-01 15:34:01 +0200

    Pytorch Script Anfang

---
+++ pytorch_script.py
@@ -0,0 +1,13 @@
+import torch 
+import torch.nn as nn
+import torchvision.transforms as transforms
+import numpy as np
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
